7ff27ec3bfd2c7e61c1498066c755aae5974c079
louis <game.night.77176@gmail.com>
2024-12-09 15:07:26 +0100

    lexer

--- src/main/java/fr/tanchoulet/bakefile/Lexer.java
+++ src/main/java/fr/tanchoulet/bakefile/Lexer.java
@@ -3,8 +3,13 @@ package fr.tanchoulet.bakefile;
 import java.util.List;
 
 public class Lexer {
+    private final String input;
 
-    List<Token> tokenize(String input) {
+    public Lexer(String input) {
+        this.input = input;
+    }
+
+    List<Token> tokenize() {
 
         return null;
     }
